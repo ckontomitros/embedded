7f0f435946ce4c77f4447eea6e5ccbf72633754f
kontotrelos <chkon2009@yahoo.gr>
2017-02-22 11:57:18 +0200

    Ngray

--- Documents/embeded/lab1_input/step3/verilog_input/GrayCounter_Nbits.v
+++ Documents/embeded/lab1_input/step3/verilog_input/GrayCounter_Nbits.v
@@ -8,15 +8,20 @@ module gray_Nbits (clk, clk_en, rst, gray_out);
   parameter N = 4;
   parameter SIZE =(N+1);
   parameter Zeros = {SIZE{1'b0}};
+  integer i;
   // Place the definition of wires and regs here
-  
+  reg [N:0] state,toggle;
+  input clk,clk_en,rst;
+  output gray_out;
+
   // The state of the Gray counter
   always @(posedge clk, negedge rst)
     begin
 	     if (rst == 1'b0)
-		    // Initialize state with 1000..00
+		    state=Zeros;
+		    toggle={N{1'b0},1};
 	     else
-		    // Main part of code
+		    state<=state^toggle;
 	 end
   
   
@@ -27,8 +32,9 @@ module gray_Nbits (clk, clk_en, rst, gray_out);
         toggle[1] <= state[0];	
         for (i=2; i<N; i=i+1) 
 	   begin	
-            // Here goes your code	   
+            toggle[i]<=toggle[i-1]&&(~|toggle[i-2:0]);   
            end 
+         toggle[N]<=(~|toggle[N-1:0]);
 	  
     assign gray_out=state[N:1];
 	 
