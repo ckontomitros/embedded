a36e7ce543e9c932732137818ebd9445339ca192
kontotrelos <chkon2009@yahoo.gr>
2017-02-24 10:08:37 +0200

    nbitsgray

--- lab1_input/step2/verilog_input/GrayCounter_Nbits.v
+++ lab1_input/step2/verilog_input/GrayCounter_Nbits.v
@@ -6,47 +6,26 @@
 
 module gray_Nbits (clk, clk_en, rst, gray_out);
   parameter N = 4;
-  parameter SIZE =(N+1);
-  parameter Zeros = {SIZE{1'b0}};
-  integer i,j;
+  integer i;
+  reg [N-1:0] counter;
   // Place the definition of wires and regs here
-  input clk,clk_en,rst;
-  output[N:1] gray_out;
-  reg [N:0] state;
-  reg [N:0] toggle;
-  reg acc;
+  output [N-1:0] gray_out;
+  input clk,rst,clk_en;
   // The state of the Gray counter
   always @(posedge clk, negedge rst)
     begin
-	     if (rst == 1'b1)
-	     	begin
-		   state=Zeros; // Initialize state with 1000..00
-		   toggle={Zeros[N-1:0],1'b1};
-		   acc=0;
-		end
+	     if (rst == 1'b1)begin
+		    // Initialize state with 1000..00
+        counter=0;
+        state=0;
+        end
 	     else
-	      	
-		    state<=state^toggle;
-
+		    // Main part of code
+        counter<={1'b0,counter[N-1:1]}^gray_out[N-1:0];
+        counter<=counter+1'b1;
 	 end
   
   
   // The combinational logic produces the toggle[N:0] signals
-  always @(state) 
-    begin     
-        toggle[0] <=  1'b1;
-        toggle[1] <= state[0];	
-        for (i=2; i<N; i=i+1) 
-	   	  begin	
-	   	  	acc<=0;
-	   		for (j=0; j<=i-2; j=j+1) begin
-	   			
-	   			acc<=acc||state[j];
-	   		end
-            toggle[i]<=state[i-1]&&(~(acc));   
-           end 
-	   toggle[N]<=~|state[N-1:0];
-	end
-    assign gray_out=state[N:1];
-	 
+  assign gray_out={1'b0,counter[N-1:1]}^counter[N-1:0];
 endmodule
